bf61ac3db7978bb2e23e8e4f001c42fd48d9fb3d
thewhiskas27 <daromir63@gmail.com>
2025-06-19 12:48:51 +0200

    Finition de la classe getWinners

--- SpecFlowTP2API/Scrutin.cs
+++ SpecFlowTP2API/Scrutin.cs
@@ -14,23 +14,44 @@ public class Scrutin
         return votes;
     }
     public int getVotes (Candidat c) { return c.votes; }
-    public List<Candidat> getWinner(List<Candidat> cs)
+    public List<Candidat> getWinners(List<Candidat> cs)
     {
-        Candidat cwinner = null;
-        Candidat sec = null;
-        List<Candidat> winners = new List<Candidat>();
-        winners.Add(cwinner);
-        winners.Add(sec);
-        int votes = getAllVotes(cs);
-        var vcand = 0;
-        foreach (var c in cs)
+        switch (cs.Count)
         {
-            vcand += c.votes * 100 / votes;
-            if (vcand > 50)
-            {
-                winners.Remove(sec);
+            case < 2:
+                return cs;
+            case 2:
+                if (cs[0].votes > cs[1].votes) cs.Remove(cs[1]);
+                else if (cs[0].votes < cs[1].votes) cs.Remove(cs[0]);
+                else cs.Clear();
+                return cs;
+            default:
+                Candidat first = null;
+                Candidat sec = null;
+                List<Candidat> winners = new List<Candidat> { first, sec };
+                int votes = getAllVotes(cs);
+                var vcand = 0;
+                foreach (var c in cs)
+                {
+                    if (sec.votes >= c.votes) continue;
+                    vcand += c.votes * 100 / votes;
+                    if (vcand > 50)
+                    {
+                        winners.Remove(sec);
+                        first = c;
+                        return winners;
+                    }
+                    else
+                    {
+                        if (c.votes > first.votes)
+                        {
+                            sec = first;
+                            first = c;
+                        }
+                        else sec = c;
+                    }
+                }
                 return winners;
-            }
         }
     }
 }
